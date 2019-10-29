2eed08e1614dbd43fdaacbdc8f8f1379fd9c3c1e
Dhuha Hasan <duha_9@yahoo.com>
2019-10-29 10:46:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function testFunc() {
 }
 
 function superbowlWin(record){
-  
-  if ((record.find(obj=> obj.result==="W"))!==undefined) {return winResult.year;} else {return undefined;}
+  let winResult=
+  if ((record.find(obj=> obj.result==="W"))!==undefined) {return .year;} else {return undefined;}
    
 }
