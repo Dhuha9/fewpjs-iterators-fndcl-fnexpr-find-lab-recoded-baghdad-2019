7b5952be68c8ead896c61e0ee7981bec04b06600
Dhuha Hasan <duha_9@yahoo.com>
2019-10-29 09:05:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const testVar = {}
+
+function testFunc() {
+  return "hi";
+}
 const record = [
   {year: "2018", result: "L"},
   {year: "2017", result: "W"},
   {year: "2016", result: "N/A"}
 ];
-function testFunc() {
-  return "hi";
-}
-
 function superbowlWin(){
    record.find(obj=> obj.result==="W"? obj.year:"undefined");
 }
